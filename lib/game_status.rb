8862c3a5ee3f11c0cad6a31f45686acc50437a9a
Marci Clark <marcig612@gmail.com>
2019-08-01 13:01:48 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -56,4 +56,20 @@ def over?(board)
 end
 
 def winner(board)
+  if won?(board) == false
+    return nil 
 end 
+
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
